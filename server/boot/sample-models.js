05ea9096a84de6f6e1b571c9669f9f5320126138
Erik Podetti <erik.podetti@gmail.com>
2016-01-12 18:33:32 +0100

    Configured passport for local auth and two users

---
+++ server/boot/sample-models.js
@@ -0,0 +1,74 @@
+var async = require('async');
+module.exports = function(app, cb)
+{
+	//data sources
+	var mongolab = app.dataSources.mongolab;
+	var dbMemory = app.dataSources.db;
+
+	//data vars
+	var users = [];
+	var roles = [];
+
+	// create users
+	mongolab.automigrate('AppUser', function(err) {
+		if (err) return cb(err);
+		var User = app.models.AppUser;
+		// check if user 0 (Erik) exists
+		var where = { email : 'erik.podetti@gmail.com' };
+		User.findOne({where : where}, function(err, user){
+			if( user === null)
+			{
+					User.create(
+						{username: 'Erik', email: 'erik.podetti@gmail.com', password: 'opensesame'}
+					, function(err, user){
+						users[0] = user; //store it do data vars
+						console.log('Created users: ', user);
+
+						/*dbMemory.automigrate('Museum', function(err){
+						if(err) return cb(err);
+
+						users[0].museums.create({
+							name: 'Primo Museo',
+							description: 'Descrizione del primo museo',
+							isOpen : true
+						}, function(err, museum){
+							museums[0] = museum;
+							console.log('Created museum:', museum);
+						});
+						});*/
+					});
+			}
+		});
+		// check if user 1 (VRDev) exists
+		where = { email : 'dev@augmenta.it' };
+		User.findOne({where : where}, function(err, user){
+		if (user === null)//if not create it
+		{
+			User.create(
+			{username: 'VRDev', email: 'dev@augmenta.it', password: 'PieroAugmenta15'}
+			, function(err, user){
+			users[1] = user; //store it to data vars
+				console.log('Created users: ', user);
+				//create the admin role
+				mongolab.autoupdate('Role', function(err) {
+				if (err) return cb(err);
+					var Role = app.models.Role;
+					Role.create({
+					name: 'admin'
+					}, function(err, role){
+					if (err) return(err);
+						// Make user[1] an admin
+						var RoleMapping = app.models.RoleMapping;
+						role.principals.create({
+						principalType: RoleMapping.USER,
+							principalId: users[1].id
+						});
+					});
+				});
+			});
+		}
+		});
+	});
+};
+
+
--- server/server.js
+++ server/server.js
@@ -3,6 +3,87 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+var loopbackPassport = require('loopback-component-passport');
+var PassportConfigurator = loopbackPassport.PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+
+/*
+ * body-parser is a piece of express middleware that
+ *   reads a form's input and stores it as a javascript
+ *   object accessible through `req.body`
+ *
+ */
+var bodyParser = require('body-parser');
+
+/**
+ * Flash messages for passport
+ *
+ * Setting the failureFlash option to true instructs Passport to flash an
+ * error message using the message given by the strategy's verify callback,
+ * if any. This is often the best approach, because the verify callback
+ * can make the most accurate determination of why authentication failed.
+ */
+var flash = require('express-flash');
+
+// attempt to build the providers/passport config
+var config = {};
+try {
+	config = require('../providers.json');
+} catch (err) {
+	console.trace(err);
+	process.exit(1); // fatal
+}
+
+// boot scripts mount components like REST API
+boot(app, __dirname, function(err) {
+	if (err) throw err;
+ });
+
+// -- Add your pre-processing middleware here --
+
+// BODY PARSER
+// to support JSON-encoded bodies
+app.middleware('parse', bodyParser.json());
+// to support URL-encoded bodies
+app.middleware('parse', bodyParser.urlencoded({
+	extended: true
+}));
+
+// ACCESS TOKEN
+// The access token is only available after boot
+app.middleware('auth', loopback.token({
+  model: app.models.accessToken,
+  currentUserLiteral: 'me'
+}));
+
+// COOKIE PARSER
+app.middleware('session:before', loopback.cookieParser(app.get('cookieSecret')));
+app.middleware('session', loopback.session({
+	secret: 'kitty',
+	saveUninitialized: true,
+	resave: true
+}));
+
+// FLASH PASSPORT ERROR MESSAGES
+app.use(flash());
+
+// PASSPORT CONFIGURATORS
+passportConfigurator.init();
+	passportConfigurator.setupModels({
+		userModel : app.models.AppUser,
+		userIdentityModel : app.models.userIdentity,
+		userCredentialModel : app.models.userCredential
+	});
+
+for (var s in config) {
+	var c = config[s];
+	c.session = c.session !== false;
+	passportConfigurator.configureProvider(s, c);
+}
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
@@ -16,12 +97,7 @@ app.start = function() {
   });
 };
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
-
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
+if (require.main === module){
+	app.start();
+}
